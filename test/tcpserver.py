73140cd1f2d25728e2ceacc8fd116250cda5293c
Holland Gibson <holland.gibson@gmail.com>
2015-09-27 12:47:39 -0500

    Server side config

--- test/tcpserver.py
+++ test/tcpserver.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
-import socket
+import socket, ssl
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.0.10'
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
@@ -10,9 +10,14 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
+wrapper = ssl.wrap_socket(conn, 
+			  server_side=True,
+			  certfile="/root/ca/intermediate/certs/www.web.arcnet.cert.pem",
+			  keyfile="/root/ca/intermediate/private/www.web.arcnet.key.pem")
 print('Connection address:', addr)
 while 1:
-    data = conn.recv(BUFFER_SIZE)
+   # data = conn.recv(BUFFER_SIZE)
+    data = wrapper.recv(BUFFER_SIZE)
     if not data: break
     print("received data:", data.decode())
     conn.send(data)  # echo
