ad1620184c435ad811564e4efe566056cbbf4adb
Holland <holland@hackintosh.arcnet>
2015-11-14 19:29:41 -0600

    Debugging

--- hazc_device.py
+++ hazc_device.py
@@ -3,34 +3,108 @@ from zeroconf import Zeroconf, ServiceInfo
 import socket
 import configparser
 
-class hazc_master:
+class hazc_device:
     def __init__(self, ipaddr):
+        self.version = "0.1"
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
         self.MSGLEN = 1024
         self.END_OF_MSG = '*'
         self.ip = ipaddr
-        
+        self.buffer = 20
+        self.commands = {'version?':self.version_cmd,'commands?':self.commands_cmd,'status?':self.status_cmd}
+
+    def addFunction(self, name, handler, statushandler):
+        self.commands
+
     def advertise(self):
         postfix = self.config['global']['service_prefix']
-        port = int(self.config['global']['port'])
-        info = ServiceInfo(postfix, self.config['device']['hostname']+ postfix,
-                       socket.inet_aton(self.ip), port, 0, 0,
-                       self.config['device']['description'], "hazc.local.")
-                       
+        self.port = int(self.config['global']['port'])
+        #print(self.config['device']['hostname']+postfix)
+        info = ServiceInfo(postfix, self.config['device']['hostname']+postfix,
+                       socket.inet_aton(self.ip), self.port, 0, 0,
+                       {'info': self.config['device']['description']}, "hazc.local.")
+
+        self.bindConnection()
+
         zeroconf = Zeroconf()
         zeroconf.register_service(info)
-        
+
+
         try:
             while True:
-                self.listen()
+                try:
+                    print("Ready")
+                    self.conn, self.addr = self.webcontrol.accept()
+                    self.listen()
+                finally:
+                    self.conn.close()
         except KeyboardInterrupt:
             pass
         finally:
+            print()
             print("Unregistering...")
             zeroconf.unregister_service(info)
             zeroconf.close()
-            
+
+        try:
+            print("Shutting down socket")
+            self.webcontrol.shutdown(socket.SHUT_RDWR)
+        except Exception as e:
+            print(e)
+
     def listen(self):
-        
-        
+        data = bytes()
+        rbytes = 0
+        while rbytes < self.MSGLEN:
+            d = self.conn.recv(self.buffer)
+            if not d: break
+            data += d
+            rbytes += len(d)
+
+#         print data.decode('utf-8')
+        print(data)
+        self.handledata(data)
+
+    def handledata(self, data):
+        command = self.cleanandstringdata(data)
+
+        replystr = "ERROR"
+
+        replystr = self.commands[command]()
+
+        print(replystr)
+        self.reply(replystr)
+
+
+    def reply(self, msg):
+        longmsg = msg
+        while len(longmsg) < self.MSGLEN:
+            longmsg += self.END_OF_MSG
+#         print(longmsg)
+        self.conn.send(longmsg.encode('utf-8'))
+
+    def cleanandstringdata(self, data):
+        dstr = data.decode('utf-8')
+        return dstr.strip(self.END_OF_MSG)
+
+    def bindConnection(self):
+        try:
+            self.webcontrol = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.webcontrol.bind((self.ip, self.port))
+            self.webcontrol.listen(1)
+        except OSError as e:
+            print(e)
+            quit()
+
+    def version_cmd(self):
+        return self.version
+
+    def commands_cmd(self):
+        rstr = ""
+        for key, value in self.commands:
+            rstr += key + ";"
+        return rstr
+
+    def status_cmd(self):
+        return "some status..."
--- hazc_master.py
+++ hazc_master.py
@@ -11,34 +11,32 @@ import os.path
 class hazc_master:
 #     global inst
 
-    global instance
-
     def __init__(self): #, ipaddr):
 #         global instance
 #         instance = self
-        
+
         #ipaddr = 'localhost'
-    
+        self.version = "0.1"
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
         #self.ip = ipaddr
         self.MSGLEN = 1024
         self.END_OF_MSG = '*'
-        
+
         self.xmlpath = self.config['discovery']['xml_location']
         self.checkXML()
-        
+
 #         self.xmlroot = ET.Element("root")
-        
+
 #         global inst
 #         inst = self
         #init the XML file
-        
+
     #start searching - THE method to run
     def detectDevices(self):
         self.zeroconf = Zeroconf()
-        self.listener = hazcListener()
-        self.browser = ServiceBrowser(self.zeroconf, self.config['global']['service_prefix'], hazcListener)
+        self.listener = hazcListener(self)
+        self.browser = ServiceBrowser(self.zeroconf, self.config['global']['service_prefix'], self.listener)
         try:
             input("Press enter to exit...\n\n")
 #             while(1):
@@ -46,25 +44,26 @@ class hazc_master:
         finally:
             self.zeroconf.close()
 #             self.webcontrol.close() #May not be neccesary
-        
+
         #self.bindConnection() #option2
-    
+
     def bindConnection(self):
         self.webcontrol = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.webcontrol.bind(('localhost', 8796))
-        self.webcontrol.listen(1)    
-    
+        self.webcontrol.listen(1)
+
 #     TODO: send variable length packets
     def getInfo(self, ip):
+        print("Getting info...")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((str(ip),int(config['global']['port'])))
-        
+        s.connect((ip,int(self.config['global']['port'])))
+
         attr = {}
-        
+
         self.senddata(s, "version?")
-        version = recvdata(s)
+        version = self.recvdata(s)
         attr['version'] = version
-        
+
         self.senddata(s, "commands?")
         #retrieve commands as according to README
         commands = recvdata(s)
@@ -73,75 +72,86 @@ class hazc_master:
         self.senddata(s, "status?")
         # control surfaces
         controls = list()
-        
+
         attr['controls'] = controls
-        
+
         s.shutdown(1)
         s.close()
-        
+
         return attr
-        
+
     def parseconfigs(self, cmdlist):
         confs = list()
         for str in cmdlist:
             if(str[0:4] is "set-"):
                 confs.append(str[4:])
         return confs
-        
+
     def fixmsglength(self, msg):
-        if msg < self.MSGLEN:
+        if len(msg) < self.MSGLEN:
+#             print("not long enough")
             while len(msg) < self.MSGLEN:
-                msg.append(self.END_OF_MSG)
+                msg+=self.END_OF_MSG
+            return msg
         else:
+#             print("too long")
             return msg[0:self.MSGLEN]
-        
+
     def senddata(self, sock, msg):
-        msg = self.fixmsglength(msg)
+        print(msg)
+        bmsg = self.fixmsglength(msg).encode('utf-8')
         totalsent = 0
         while totalsent < self.MSGLEN:
-            sent = sock.send(msg[totalsent:])
+#             print(totalsent)
+#             print(msg)
+            sent = sock.send(bmsg[totalsent:])
             if sent == 0:
                 # closed
-                raise RuntimeError("socket connection broken unexpectedly. Was trying:" + str(msg))
+                raise RuntimeError("socket connection broken unexpectedly. Was trying:" + str(bmsg.decode('utf-8')))
             totalsent = totalsent + sent
-    
+
     def recvdata(self, sock):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < self.MSGLEN:
-            chunk = sock.recv(MSGLEN - bytes_recd)
-        if chunk == b'':
-            raise RuntimeError("Socket connection broken unexpectedly")
-        chunks.append(chunk)
-        bytes_recd += len(chunk)
-        
-        msg = b''.join(chunks)
-        return msg.strip(self.END_OF_MSG)
-        
+#         chunks = []
+#         bytes_recd = 0
+#         while bytes_recd < self.MSGLEN:
+#             chunk = sock.recv(self.MSGLEN - bytes_recd)
+#         if chunk == b'':
+#             raise RuntimeError("Socket connection broken unexpectedly")
+#         chunks.append(chunk)
+#         bytes_recd += len(chunk)
+#
+#         msg = b''.join(chunks)
+
+        msgbytes = sock.recv(self.MSGLEN)
+        msgstr = msgbytes.decode('utf-8')
+        rmsg = msg.strip(self.END_OF_MSG)
+        print("->" + rmsg)
+        return rmsg
+
     def checkXML(self): #see if XML exists, otherwise create it.
 #         root = ET.Element("root")
         #if(not os.path.isfile(self.xmlpath)):
-        
+
         #Set up a new clean XML - as loading one may have old devices
         devices = ET.Element('devices')
         tree = ET.ElementTree(devices)
 #             root = ET.Element('devices')
         tree.write(self.xmlpath)
-        
+
     def add_service_xml(self, info):
         tree = ET.parse(self.xmlpath)
         devices = tree.getroot()
-        
-        cleanname = info.split(self.config['global']['service_prefix'])[0]
+
+        cleanname = info.name.split(self.config['global']['service_prefix'])[0]
         newservice = ET.SubElement(devices, cleanname)
-        
-        
+
+
 #         tree = ET.ElementTree(self.xmlroot)
         deviceAttribs = self.getInfo(socket.inet_ntoa(info.address))
-        
+
         version = ET.SubElement(newservice, "version")
         version.text = deviceAttribs['version']
-        
+
         control = ET.SubElement(newservice, "control")
         for controlsurface in deviceAttribs['controls']:
             newsurface = ET.SubElement(control, controlsurface['name'])
@@ -152,30 +162,32 @@ class hazc_master:
                 enum.text = controlsurface['enum']
             value = ET.SubElement(newsurface, "value")
             value.text = controlsurface['value']
-        
 
-        
+
+
 #         devices.append(newdevice)
         tree.write(self.xmlpath)
-        
-    def remove_service(self, info):
+
+    def remove_service_xml(self, info):
         tree = ET.parse(self.xmlpath)
         devices = tree.getroot()
-        
+
         cleanname = info.split(self.config['global']['service_prefix'])[0]
-        
+
         devices.remove(devices.find(cleanname))
         tree.write(self.xmlpath)
 
 class hazcListener(object):
+    def __init__(self, hmaster):
+        self.hazc_master = hmaster
 
     def remove_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
-        hazc_master.instance.remove_service_xml(info)
+        self.hazc_master.remove_service_xml(info)
         print("Service %s removed" % (name,))
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
         print("Service %s added, service info: %s" % (name, info))
         # Add to XML
-        hazc_master.instance.add_service(info)
+        self.hazc_master.add_service_xml(info)
